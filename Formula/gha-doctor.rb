0d9ece4dfc7cbd307c7fc906ca53f2b708ef90d1
Linnea Bakshi <oc-bd1838@agentmail.to>
2026-07-28 16:58:13 +0000

    gha-doctor 0.4.0: shell completions (installed automatically)

--- Formula/gha-doctor.rb
+++ Formula/gha-doctor.rb
@@ -5,31 +5,32 @@
 class GhaDoctor < Formula
   desc "Diagnose GitHub Actions: flaky jobs, wasted minutes, cache misses, anti-patterns"
   homepage "https://github.com/linnea-bakshi/gha-doctor"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.3.0/gha-doctor_0.3.0_darwin_arm64.tar.gz"
-      sha256 "78e1b136f3bdc2d234fd7df18ba2714dac73babd763bc029c1bc25c332ba5f17"
+      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.4.0/gha-doctor_0.4.0_darwin_arm64.tar.gz"
+      sha256 "d4aacd16b670e952840aa91f5a1c1465135e3c3841a8f7295ae156f77ce65ceb"
     else
-      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.3.0/gha-doctor_0.3.0_darwin_amd64.tar.gz"
-      sha256 "d7bbbd6506b31b23037ca31b9cf7e1ef4c3cdd00d2ad84d7f49be63c799e164d"
+      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.4.0/gha-doctor_0.4.0_darwin_amd64.tar.gz"
+      sha256 "ba1f1f0497652bdfe2cae42fc49c7cf01bafa137493df610a6ed73ec5693ddd6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.3.0/gha-doctor_0.3.0_linux_arm64.tar.gz"
-      sha256 "48fb2ea6083f8ffb617fc5b9f30bc9cd408b99b00852cca40cd30f190ecc05d2"
+      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.4.0/gha-doctor_0.4.0_linux_arm64.tar.gz"
+      sha256 "0a696147df8f3789e27fb5dae02e978746b52a8e62c15824755753004316e99d"
     else
-      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.3.0/gha-doctor_0.3.0_linux_amd64.tar.gz"
-      sha256 "5cdf8525f0fc4f6d003deaecb3f282add96caada4e764f93abb6f734fa252184"
+      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.4.0/gha-doctor_0.4.0_linux_amd64.tar.gz"
+      sha256 "63f64c6b14e83faff7de4e0e422f673c452e90e4a5b8299e7a4f9fcbb4392f99"
     end
   end
 
   def install
     bin.install "gha-doctor"
+    generate_completions_from_executable(bin/"gha-doctor", "--completion")
   end
 
   test do
