abf0c671688044466d191f317c5dc05749a06a04
Linnea Bakshi <oc-bd1838@agentmail.to>
2026-07-30 10:08:44 +0000

    gha-doctor 0.15.1

--- Formula/gha-doctor.rb
+++ Formula/gha-doctor.rb
@@ -5,26 +5,26 @@
 class GhaDoctor < Formula
   desc "Diagnose GitHub Actions: flaky jobs, wasted minutes, cache misses, anti-patterns"
   homepage "https://github.com/linnea-bakshi/gha-doctor"
-  version "0.15.0"
+  version "0.15.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.15.0/gha-doctor_0.15.0_darwin_arm64.tar.gz"
-      sha256 "414ab6a0f16cc182c07e4303b0da2b56f95bb33bcdc2b83659e461bcfc00505d"
+      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.15.1/gha-doctor_0.15.1_darwin_arm64.tar.gz"
+      sha256 "7dc409e9a1f0e2a35489c84f161d1706d801453c72a31e98d42184323653c650"
     else
-      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.15.0/gha-doctor_0.15.0_darwin_amd64.tar.gz"
-      sha256 "ed6526ce2a4409e0a2374b0fad26139315085bebafa66f638b7c963e708b4ca5"
+      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.15.1/gha-doctor_0.15.1_darwin_amd64.tar.gz"
+      sha256 "64b2d36535eba11200e39466935f8607ab3be8ff03d14da996b1a3551eae28bd"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.15.0/gha-doctor_0.15.0_linux_arm64.tar.gz"
-      sha256 "babd0d65294dea89787e9d3ed49ac00c78512a92c3aad10b9909ec5bc50e45a7"
+      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.15.1/gha-doctor_0.15.1_linux_arm64.tar.gz"
+      sha256 "a75ed224e03c5c60ea0ee811e96346c7c5bd63c284cb0dd390e8dd06df60b62a"
     else
-      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.15.0/gha-doctor_0.15.0_linux_amd64.tar.gz"
-      sha256 "69c08aba3c660e0e646949a20d237db30bb1be3a3b2de5facfe2f51d3b697080"
+      url "https://github.com/linnea-bakshi/gha-doctor/releases/download/v0.15.1/gha-doctor_0.15.1_linux_amd64.tar.gz"
+      sha256 "d72dd6b865e82f0cf8f295815089f7148c3eba5c271eaf4a9a7a18bc16fec2a1"
     end
   end
 
